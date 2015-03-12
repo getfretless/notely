45fda891cac0f6422f0f0aff2ead4773c282362a
David Jones <unixmonkey1@gmail.com>
2015-03-12 12:29:35 -0400

    Retrieve notes from API

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var notelyBasePath = 'https://elevennote-nov-2014.herokuapp.com/api/v1/';
+var apiKey = '$2a$10$Z96eCeXE/kPt/l1Yuvg5xuJr1MArnxV33yJ2z0hjBcVZZCiJtHwZa';
+
 angular.module('myApp.notes', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,6 +12,9 @@ angular.module('myApp.notes', ['ngRoute'])
   });
 }])
 
-.controller('NotesController', [function() {
-
+.controller('NotesController', ['$http', '$scope', function($http, $scope) {
+  $http.get(notelyBasePath + 'notes.json?api_key=' + apiKey)
+  .success(function(notes_data) {
+    $scope.notes = notes_data;
+  });
 }]);
