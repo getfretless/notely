f3b561c78637dc7817d29689a8430aaec001315a
David Jones <unixmonkey1@gmail.com>
2015-03-13 09:39:24 -0400

    Added custom directive to focus on new note and added callback to return note on post

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -33,9 +33,9 @@ angular.module('myApp.notes', ['ngRoute'])
       NotesBackend.updateNote($scope.note);
     }
     else {
-      NotesBackend.postNote($scope.note);
-      // TODO: keep note and set $scope.note to the persisted object
-      $scope.note = {};
+      NotesBackend.postNote($scope.note, function(newNote) {
+        $scope.note = JSON.parse(JSON.stringify(newNote));
+      });
     }
   };
 
@@ -54,6 +54,10 @@ angular.module('myApp.notes', ['ngRoute'])
 
   $scope.clearNote = function() {
     $scope.note = {};
+    // These do the same thing, but the broadcast listener in the
+    // focusOn directive provides a cleaner separation of code from HTML
+    // document.getElementById('note_title').focus();
+    $scope.$broadcast('noteCleared');
   };
 
 }])
@@ -74,12 +78,13 @@ angular.module('myApp.notes', ['ngRoute'])
     });
   };
 
-  this.postNote = function(noteData) {
+  this.postNote = function(noteData, callback) {
     $http.post(notelyBasePath + 'notes', {
       api_key: apiKey,
       note: noteData
     }).success(function(newNoteData) {
       notes.push(newNoteData);
+      callback(newNoteData);
     });
   };
 
